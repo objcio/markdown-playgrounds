32c1c396a61e73beeb6895e6bb77574926656928
Chris Eidhof <chris@eidhof.nl>
2019-03-19 10:55:42 +0100

    splitview

--- Sources/playground-simplified/Boilerplate.swift
+++ Sources/playground-simplified/Boilerplate.swift
@@ -17,6 +17,20 @@ func application(delegate: AppDelegate) -> NSApplication {
     return app
 }
 
+func splitView(_ views: [NSView]) -> NSSplitView {
+    let sv = NSSplitView()
+    sv.isVertical = true
+    sv.dividerStyle = .thin
+    for v in views {
+        sv.addArrangedSubview(v)
+    }
+    sv.setHoldingPriority(.defaultLow - 1, forSubviewAt: 0)
+    sv.autoresizingMask = [.width, .height]
+    sv.autosaveName = "SplitView"
+    return sv
+
+}
+
 extension NSTextView {
     func configureAndWrapInScrollView(isEditable editable: Bool, inset: CGSize) -> NSScrollView {
         let scrollView = NSScrollView()
