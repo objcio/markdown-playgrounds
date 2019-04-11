8f07d2c7e530b728f4ee711253bb60a62f856a86
Chris Eidhof <chris@eidhof.nl>
2019-04-11 13:56:17 +0200

    Swift Highlighting

--- Sources/MarkdownPlaygroundsLibrary/MarkdownHighlighting.swift
+++ Sources/MarkdownPlaygroundsLibrary/MarkdownHighlighting.swift
@@ -66,12 +66,85 @@ extension NSMutableAttributedString {
             case CMARK_NODE_CODE_BLOCK:
                 addAttribute(.backgroundColor, value: NSColor.windowBackgroundColor, range: nsRange)
                 addAttribute(.font, value: NSFont(name: "Monaco", size: attributes.size)!, range: nsRange)
-                codeBlocks.append(CodeBlock(text: c.literal!, range: nsRange))
+                let code = c.literal!
+                codeBlocks.append(CodeBlock(text: code, range: nsRange))
+                if let tokens = try? code.highlightSwift() {
+                    let offset = nsRange.location + (string[start...end] as NSString).range(of: code).location
+                    for token in tokens {
+                        var range = NSRange(token.range, in: code)
+                        range.location += offset
+                        addAttribute(.foregroundColor, value: token.kind.color, range: range)
+                    }
+                }
             default:
                 break
             }
         }
         return codeBlocks
+    }
+}
+
+struct Token {
+    var range: Range<String.Index>
+    var kind: Kind
+    
+    enum Kind {
+        case keyword
+        case string
+        case number
+    }
+}
+
+extension Token.Kind {
+    var color: NSColor {
+        switch self {
+        case .keyword: return accentColors[4]
+        case .string: return accentColors[2]
+        case .number: return accentColors[3]
+        }
+    }
+}
+
+import SwiftSyntax
+
+final class SwiftHighlighterRewriter: SyntaxRewriter {
+    var tokens: [Token] = []
+    let code: String
+    
+    init(code: String) {
+        self.code = code
+    }
+    
+    override func visit(_ token: TokenSyntax) -> Syntax {
+        var range: Range<String.Index> {
+            let start = code.utf8.index(code.utf8.startIndex, offsetBy: token.positionAfterSkippingLeadingTrivia.utf8Offset)
+            let end = code.utf8.index(code.utf8.startIndex, offsetBy: token.endPosition.utf8Offset)
+            return start..<end
+        }
+        switch token.tokenKind {
+        case .stringSegment, .stringQuote, .stringLiteral:
+            tokens.append(Token(range: range, kind: .string))
+        case .integerLiteral, .floatingLiteral:
+            tokens.append(Token(range: range, kind: .number))
+        case let kind where kind.isKeyword:
+            tokens.append(Token(range: range, kind: .keyword))
+        default:
+            ()
+        }
+        return token
+    }
+}
+
+extension String {
+    func highlightSwift() throws -> [Token] {
+        let tempDir = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+        let fileName = "\(UUID().uuidString).swift"
+        let file = tempDir.appendingPathComponent(fileName)
+        try write(to: file, atomically: true, encoding: .utf8)
+        let parsed = try SyntaxTreeParser.parse(file)
+        let highlighter = SwiftHighlighterRewriter(code: self)
+        _ = highlighter.visit(parsed)
+        return highlighter.tokens
     }
 }
 
