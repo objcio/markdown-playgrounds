0858dc22838cb72bd101bd240dbfe289a35f94c4
Chris Eidhof <chris@eidhof.nl>
2019-04-12 19:53:17 +0200

    Bump to SwiftSyntax 0.50000.0

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/objcio/commonmark-swift", .branch("swift-5")),
-        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.40200.0")),
+        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50000.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
